67dd78bcd581c01a2d55cd8524865d741a46bf0a
Leonardo Montini <leonardo.montini@flowing.it>
2022-07-15 12:02:12 +0200

    Added default parameter with empty array

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -12,9 +12,27 @@ describe('iswitch', () => {
         },
       ]),
     ).toBe(true);
+
+    expect(iswitch('b', ['a', () => 'A'], ['b', () => 'B'], ['c', () => 'C'])).toBe('B');
+  });
+
+  it('should handle cases with multiple keys', () => {
+    expect(iswitch('a', ['x', () => false], [['a', 'b'], () => true])).toBe(true);
   });
 
   it('should return null if there are no matches', () => {
     expect(iswitch('a', ['X', () => 'A'])).toBe(undefined);
+  });
+
+  it('should handle the default there are no matches', () => {
+    expect(iswitch('a', ['X', () => 'A'], [[], () => 'default'])).toBe('default');
+  });
+
+  it('should throw if there are duplicate keys', () => {
+    expect(() => iswitch('a', ['X', () => 'A'], [['X'], () => 'default'])).toThrowError(/^Duplicate keys: X$/);
+
+    expect(() => iswitch('a', [['X', 'Y'], () => 'A'], [['X'], () => 'X'], [['Y'], () => 'Y'])).toThrowError(
+      /^Duplicate keys: X, Y$/,
+    );
   });
 });
